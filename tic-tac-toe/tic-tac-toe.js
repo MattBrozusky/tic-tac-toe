3baa897632cd53a27a071c2eb66976bffd061310
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-30 16:36:51 -0600

    Finished all box styling and win checks

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -5,47 +5,62 @@ $(document).ready(function () {
 
 
 
-
-    var move = 1;
-    var play = true;
-
-    $(".box").on("click", function () {
-        if (move % 2 !== 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "Y")) {
-
-
+    const box = $(".box");
+    let move = 1;
+    let play = true;
+    
+    box.on("click", function () {
+    
+        if (move % 2 !== 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "O")) {
                 $(this, "span").append("X");
-                if ($(this).html("X") || $(this).html("Y")) {
-                    $('.this').off('click');
+                if ($(this, "span").html("X")) {
+                    $(this).off('click');
                 }
                 move += 1;
-
-        } else if (move % 2 === 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "Y")) {
-
-
-                $(this, "span").append("Y");
-                if ($(this).html("Y") || $(this).html("X")) {
-                    $('.this').off('click');
+    
+        } else if (move % 2 === 2 % 2 && ($(this).html() !== "X" || $(this).html() !== "O")) {
+                $(this, "span").append("O");
+                if ($(this, "span").html("O")) {
+                    $(this).off('click');
                 }
                 move += 1;
+        }
+    
+    });
+    
+    box.on("click", function checkForWinner() {
+        let box1 = $('#box1'), box2 = $('#box2'), box3 = $('#box3'), box4 = $('#box4'), box5 = $('#box5'), box6 = $('#box6'), box7 = $('#box7'), box8 = $('#box8'), box9 = $('#box9');
 
+        if (box1.html() === "X" && box2.html() === "X" && box3.html() === "X" || box1.html() === "O" && box2.html() === "O" && box3.html() === "O"){
+            console.log(`Congrats ${box1.html()}! You won!`);
+            box.off('click');
 
+        } else if (box4.html() === "X" && box5.html() === "X" && box6.html() === "X" || box4.html() === "O" && box5.html() === "O" && box6.html() === "O"){
+            console.log(`Congrats ${box4.html()}! You won!`);
+            box.off('click');
+
+        } else if (box7.html() === "X" && box8.html() === "X" && box9.html() === "X" || box7.html() === "O" && box8.html() === "O" && box9.html() === "O"){
+            console.log(`Congrats ${box7.html()}! You won!`);
+            box.off('click');
+
+        } else if (box1.html() === "X" && box4.html() === "X" && box7.html() === "X" || box1.html() === "O" && box4.html() === "O" && box7.html() === "O"){
+            console.log(`Congrats ${box1.html()}! You won!`);
+            box.off('click');
+
+        } else if (box2.html() === "X" && box5.html() === "X" && box8.html() === "X" || box2.html() === "O" && box5.html() === "O" && box8.html() === "O"){
+            console.log(`Congrats ${box2.html()}! You won!`);
+
+        } else if (box3.html() === "X" && box6.html() === "X" && box9.html() === "X" || box3.html() === "O" && box6.html() === "O" && box9.html() === "O"){
+            console.log(`Congrats ${box3.html()}! You won!`);
+
+        } else if (box1.html() === "X" && box5.html() === "X" && box9.html() === "X" || box1.html() === "O" && box5.html() === "O" && box9.html() === "O"){
+            console.log(`Congrats ${box1.html()}! You won!`);
+
+        } else if (box3.html() === "X" && box5.html() === "X" && box7.html() === "X" || box3.html() === "O" && box5.html() === "O" && box7.html() === "O"){
+            console.log(`Congrats ${box3.html()}! You won!`);
         }
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
