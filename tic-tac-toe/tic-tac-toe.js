4b7ef1ef1b5de444a1bef9afa7c489b63f6268be
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-04 10:11:13 -0600

    finished tic tac toe easy ai

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,35 +1,47 @@
 "use strict";
 $(document).ready(function () {
+// import {  } from './ai.js';
 
-//Global constants
+//Global constants and variables
     const box = $(".box");
     let move = 1;
     let XWin = $('#player-x');
     let OWin = $('#player-o');
+    const box1 = $('#box1'), box2 = $('#box2'), box3 = $('#box3'), box4 = $('#box4'), box5 = $('#box5'), box6 = $('#box6'), box7 = $('#box7'), box8 = $('#box8'), box9 = $('#box9');
+    const boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
 // Reset and play Buttons
     $('#reset').on('click', function () {
-        $('#player-btns').removeClass('invisible');
+        $('.player-btns').removeClass('invisible');
         $('#game-container').addClass('invisible');
         for (let i = 1; i <= 9; i++){
             $('#box' + i).html('');
         }
         OWin.html('');
-        box.off('click', playGame);
+        box.off('click', playGame2Player);
+        box.off('click', playGame1Player);
         box.off('click', checkForWinner);
         move = 1;
-        box.on('click', playGame);
-        box.on('click', checkForWinner);
     });
 
     $('#two-player').on('click', function () {
         $('#game-container').removeClass('invisible');
+        $('.player-btns').addClass('invisible');
         XWin.html('Your Turn!');
-        $('#player-btns').addClass('invisible');
+        box.on('click', playGame2Player);
+        box.on('click', checkForWinner);
+    });
+
+    $('#one-player').on('click', function () {
+        $('#game-container').removeClass('invisible');
+        $('.player-btns').addClass('invisible');
+        XWin.html('Your Turn!');
+        box.on('click', playGame1Player);
+        box.on('click', checkForWinner);
     });
 
 //Game Functionality
-    function playGame() {
+    function playGame2Player() {
         if (move % 2 === 1) {
             $(this).append("X");
             if ($(this).html("X")) {
@@ -51,8 +63,6 @@ $(document).ready(function () {
     }
 
     function checkForWinner() {
-        let box1 = $('#box1'), box2 = $('#box2'), box3 = $('#box3'), box4 = $('#box4'), box5 = $('#box5'), box6 = $('#box6'), box7 = $('#box7'), box8 = $('#box8'), box9 = $('#box9');
-
         if (box1.html() === "X" && box2.html() === "X" && box3.html() === "X" || box1.html() === "O" && box2.html() === "O" && box3.html() === "O"){
             results(box1);
 
@@ -81,17 +91,69 @@ $(document).ready(function () {
 
     function results(boxNum){
         if (boxNum.html() === 'X'){
-            XWin.html('Congrats! You won!');
+            XWin.html('Congrats!<br>You won!');
             OWin.html('You Lost!')
         } else {
-            OWin.html('Congrats! You won!');
+            OWin.html('Congrats!<br>You won!');
             XWin.html('You Lost!')
         }
         box.off('click');
     }
 
-//Initial Start
-    box.on('click', playGame);
-    box.on('click', checkForWinner);
+//AI for game
+
+
+    function playGame1Player() {
+        if (move % 2 === 1) {
+            $(this).append("X");
+            if ($(this).html("X")) {
+                $(this).off('click');
+            }
+            move += 1;
+            OWin.html('Your Turn!');
+            XWin.html('');
+            setTimeout(playGame1Player, 1000);
+        } else {
+            if (XWin.html() === 'Congrats!<br>You won!'){
+                return;
+            }
+
+            let aiChoice = Math.floor((Math.random() * 8));
+            while(boxes[aiChoice].html() === 'X' || boxes[aiChoice].html() === 'O'){
+                aiChoice = Math.floor((Math.random() * 8));
+            }
+            boxes[aiChoice].html('O');
+
+            move += 1;
+            XWin.html('Your Turn!');
+            OWin.html('');
+            checkForWinner();
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
